a4218a3ae3c0808a07c936228f895b54cfe7fbd4
Vikkans <143171920+Vikkans@users.noreply.github.com>
2024-02-14 10:41:12 +0100

    Add project files.

---
+++ ConsoleApp62/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace forre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tal1 = 6;
+            int tal2 = 5;
+            Console.WriteLine(Störst(tal1, tal2));
+        }
+
+        static int Störst(int tal1, int tal2)
+        {
+            if (tal1 > tal2)
+            {
+                return tal1;
+            }
+
+            else
+            {
+                return tal2;
+            }
+        }
+    }
+}
